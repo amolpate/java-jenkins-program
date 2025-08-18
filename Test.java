38eb71c921265e2c2f1dcff9ed4ad659ca5ce181
Amol Pate <amol.pate1@Indussoft.com>
2025-08-18 16:05:41 +0530

    This is test file to test jenkins throuth github

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String [] args) {
+		System.out.println("Hello");
+		System.out.println("This program is pushing into Github");
+		System.out.println("Executing this program through jenkins");
+		
+	}
+}
